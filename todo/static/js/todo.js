4b9024b6978d7b5e4b3f6c841c45a09a972e13d0
_tam <tamjid84@yahoo.com>
2018-06-20 00:33:12 +0600

    display data

--- todo/static/js/todo.js
+++ todo/static/js/todo.js
@@ -1,6 +1,16 @@
 var app = angular.module('toDo',[]);
-app.controller('toDoController', function($scope){
-    $scope.todoList = [{todoText: 'finish this app', done:false}];
+app.controller('toDoController', function($scope, $http){
+    // $scope.todoList = [{todoText: 'finish this app', done:false}];
+    $http.get('/todo/api/').then(function (response) {
+        $scope.todoList = [];
+        for (var i = 0; i < response.data.length; i++){
+            var todo = {};
+            todo.todoText = response.data[i].todo_text
+            todo.done = response.data[i].todo_done
+            $scope.todoList.push(todo);
+        }
+        console.log(response.data);
+    });
     $scope.todoAdd = function () {
       $scope.todoList.push({todoText:$scope.todoInput, done:false});
       $scope.todoInput = '';
