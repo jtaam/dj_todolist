c3c22041aaad95fc910317e3859f296f4884ac55
_tam <tamjid84@yahoo.com>
2018-06-20 00:43:54 +0600

    put todo

--- todo/todo/views.py
+++ todo/todo/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import ToDoSerializer
@@ -9,3 +10,10 @@ class ToDoView(APIView):
         todos = ToDoElements.objects.all()
         serilizers = ToDoSerializer(todos, many=True)
         return Response(serilizers.data)
+
+    def put(self, request):
+        serializer = ToDoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
