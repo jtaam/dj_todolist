3eee6126fc7ee0091d4de307e813ae5d3cdc8303
_tam <tamjid84@yahoo.com>
2018-06-20 01:24:31 +0600

    api delete

--- todo/todo/views.py
+++ todo/todo/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -5,7 +6,7 @@ from .serializers import ToDoSerializer
 from .models import ToDoElements
 
 
-class ToDoView(APIView):
+class ToDoListView(APIView):
     def get(self, request):
         todos = ToDoElements.objects.all()
         serilizers = ToDoSerializer(todos, many=True)
@@ -17,3 +18,15 @@ class ToDoView(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ToDoDetailView(APIView):
+    def get(self, request, pk):
+        todo = get_object_or_404(ToDoElements, pk=pk)
+        serializer = ToDoSerializer(todo)
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        todo = get_object_or_404(ToDoElements, pk=pk)
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
